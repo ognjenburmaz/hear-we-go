49ab8c8f63b79371e880a6f853e21b0447422de0
TheOgnjenOne <ognjenkarisik2005@gmail.com>
2025-12-30 22:50:21 +0100

    guradian angel

---
+++ frontend/hear-we-go/src/app/auth-guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated()
+    ? true
+    : router.parseUrl('/login');
+};
+
+export const guestGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return !authService.isAuthenticated()
+    ? true
+    : router.parseUrl('/home');
+};
