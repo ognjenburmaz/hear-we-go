9d82a48aae4c00d8a1e776d3d9224d976a45ad49
TheOgnjenOne <ognjenkarisik2005@gmail.com>
2025-12-30 20:02:23 +0100

    navbar, user-profile-component

--- frontend/hear-we-go/src/app/auth/auth.service.ts
+++ frontend/hear-we-go/src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap, catchError, map } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 interface AuthRequest {
@@ -22,7 +22,6 @@ export interface UserRegistrationRequest {
   password: string;
 }
 
-
 export interface User {
   id?: string;
   username: string;
@@ -31,7 +30,6 @@ export interface User {
   lastName: string;
   role?: string;
 }
-
 
 @Injectable({
   providedIn: 'root'
@@ -48,7 +46,7 @@ export class AuthService {
         tap(response => {
           const token = response.jwt;
           localStorage.setItem('authToken', token);
-          console.log('Login successdockful, token stored:', token);
+          console.log('Login successful, token stored:', token);
           this.router.navigate(['/home']);
         }),
         catchError(error => {
@@ -58,35 +56,47 @@ export class AuthService {
       );
   }
 
-
   register(registrationData: UserRegistrationRequest): Observable<User> {
     const url = `${this.apiUrl}/register`;
-
-
     return this.http.post<User>(url, registrationData)
       .pipe(
         tap(user => {
           console.log('Registration successful:', user);
-
           this.router.navigate(['/login']);
         }),
         catchError(error => {
           console.error('Registration failed:', error);
-
           throw error;
         })
       );
   }
 
+  getUserInfo() {
+    const token = localStorage.getItem('authToken');
+    if (!token) return null;
 
+    try {
+      const payload = token.split('.')[1];
+      const decodedJson = atob(payload);
+      const data = JSON.parse(decodedJson);
 
-  logout(): void {
-    localStorage.removeItem('authToken');
-    this.router.navigate(['/login']);
+      return {
+        username: data.sub,
+        role: data.role
+      };
+    } catch (e) {
+      console.error("Greška pri čitanju tokena", e);
+      return null;
+    }
   }
 
   isAuthenticated(): boolean {
     return localStorage.getItem('authToken') !== null;
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    this.router.navigate(['/login']);
   }
 
   getToken(): string | null {
