49a778e7855442bedb5ac4161263694a5adce753
Milan Maric <maricmilan0414@gmail.com>
2025-12-30 05:44:58 +0100

    [ADD] Finished frontend for content

--- frontend/hear-we-go/src/app/song/songs-get-by-album-component/songs-get-by-album-component.ts
+++ frontend/hear-we-go/src/app/song/songs-get-by-album-component/songs-get-by-album-component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Song, SongService } from '../song-service';
 @Component({
   selector: 'app-songs-get-by-album-component',
-  imports: [],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './songs-get-by-album-component.html',
   styleUrl: './songs-get-by-album-component.css',
 })
-export class SongsGetByAlbumComponent {
+export class SongsGetByAlbumComponent implements OnInit
+ {
 
+  songs:Song[]=[];
+  albumId:string|null=null;
+  constructor(private router: Router,
+      private cdr:ChangeDetectorRef,
+      private service :SongService,
+      private route:ActivatedRoute)
+  {
+
+  }
+  ngOnInit(): void 
+        {
+          this.albumId = this.route.snapshot.paramMap.get('id');
+          this.LoadAllSongs();
+        }
+        LoadAllSongs():void
+        {
+          if(this.albumId!=null){
+             this.service.getAll(this.albumId).subscribe
+          ({
+           next:(songs:Song[])=>{
+               
+                this.songs=songs;
+                this.cdr.detectChanges();
+            
+             },  
+            error:(_)=>console.log("greska")
+             })
+            }
+        }
+     navigateToAdd() {
+    this.router.navigate(['/songs/add']);
+  }
 }
