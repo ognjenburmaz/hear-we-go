50c10c283df7e3f03963524c23bf268c527ee962
ognjen karisik <ognjenkarisik2005@gmail.com>
2026-01-03 20:08:15 +0100

    notifications2

--- backend/ratings-service/src/main/java/com/streaming/ratings/config/CassandraConfig.java
+++ backend/ratings-service/src/main/java/com/streaming/ratings/config/CassandraConfig.java
@@ -19,7 +19,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Value("${spring.cassandra.port:9042}")
     private int port;
 
-    @Value("${spring.cassandra.keyspace-name:ratings_ks}")
+    @Value("${SPRING_CASSANDRA_KEYSPACE_NAME}")
     private String keyspaceName;
 
     @Override
