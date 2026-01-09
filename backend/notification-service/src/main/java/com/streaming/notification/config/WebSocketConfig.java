f68eaae1f4f105fb0b58f3dc6d0e794b4c26b3b3
ognjen karisik <ognjenkarisik2005@gmail.com>
2026-01-09 15:18:21 +0100

    web sockets for notifications implemented

--- backend/notification-service/src/main/java/com/streaming/notification/config/WebSocketConfig.java
+++ backend/notification-service/src/main/java/com/streaming/notification/config/WebSocketConfig.java
@@ -13,6 +13,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+
     }
 
     @Override
