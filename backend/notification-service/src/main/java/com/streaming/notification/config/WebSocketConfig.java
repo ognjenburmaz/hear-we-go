9f3f8c6eca5abadc8c4051729425494ada79b639
ognjen karisik <ognjenkarisik2005@gmail.com>
2026-01-11 22:18:14 +0100

    https and web sockets implemented

--- backend/notification-service/src/main/java/com/streaming/notification/config/WebSocketConfig.java
+++ backend/notification-service/src/main/java/com/streaming/notification/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost")
+                .setAllowedOriginPatterns("https://localhost", "https://localhost:*", "http://localhost")
                 .withSockJS();
     }
 }
