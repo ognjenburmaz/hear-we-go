29a6e9b0dbd8742cfc887e956f42131a12870b4e
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2026-01-23 19:48:46 +0100

    [FEAT] Conditionally render global audio player based on authentication status and implement change detection on song updates

--- frontend/hear-we-go/src/app/app.ts
+++ frontend/hear-we-go/src/app/app.ts
@@ -1,7 +1,9 @@
-import {Component, signal} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './navbar/navbar';
 import {GlobalAudioPlayer} from './global-audio-player/global-audio-player';
+import {SongChangeService} from './services/song-change-service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,20 @@ import {GlobalAudioPlayer} from './global-audio-player/global-audio-player';
   ],
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('hear-we-go');
+  protected readonly localStorage = localStorage;
+  private sub!: Subscription;
+
+  constructor(private changeService: SongChangeService, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.sub = this.changeService.trigger$.subscribe(() => {
+      this.cdr.detectChanges()
+    });
+  }
+
+
 }
